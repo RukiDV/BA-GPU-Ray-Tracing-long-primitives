977812fe3bcfc149159df22f6e2da2abc3ba8552
itzMatze <50420199+itzMatze@users.noreply.github.com>
2021-09-28 19:36:58 +0200

    fix memory spike

--- gpu_raytracing_long_primitives/ray_tracing_instances/hello_vulkan.cpp
+++ gpu_raytracing_long_primitives/ray_tracing_instances/hello_vulkan.cpp
@@ -356,7 +356,7 @@ void HelloVulkan::loadHairModel(const char* filename, cyHairFile& hairfile)
                     nvmath::vec3f(colors[pointIndex + 3 + j * 3], colors[pointIndex + 4 + j * 3],
                                   colors[pointIndex + 5 + j * 3]),
                     nvmath::vec3f(dirs[pointIndex + 3 + j * 3], dirs[pointIndex + 4 + j * 3],
-                                  dirs[pointIndex + 5 + j * 3]), 0.05f});
+                                  dirs[pointIndex + 5 + j * 3]), 0.03f});
         }
         pointIndex += (segments[i] + 1) * 3;
     }
@@ -774,7 +774,7 @@ auto HelloVulkan::objectToVkGeometryKHR(const ObjModel& model)
 //
 nvvk::RaytracingBuilderKHR::BlasInput HelloVulkan::hairToVkGeometryKHR(uint32_t index)
 {
-    VkDeviceAddress dataAddress = nvvk::getBufferDeviceAddress(m_device, m_clustersAabbBuffer[index].buffer);
+    VkDeviceAddress dataAddress = nvvk::getBufferDeviceAddress(m_device, m_clustersAabbBuffer[0].buffer);
 
     VkAccelerationStructureGeometryAabbsDataKHR aabbs{VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_GEOMETRY_AABBS_DATA_KHR};
     aabbs.data.deviceAddress = dataAddress;
@@ -807,7 +807,6 @@ std::vector<HelloVulkan::Aabb> HelloVulkan::addCluster(const uint32_t i, const u
     // TODO: this does not work if the segments are scattered in space, we need to add up all segment-vectors to get the average vector
     const nvmath::vec3f p0 = m_hairs[cluster.index].v0.p;
     const nvmath::vec3f p1 = m_hairs[cluster.index + cluster.count - 1].v1.p;
-    float scale = nvmath::length(p1 - p0);
     nvmath::vec3 unit = nvmath::vec3(0.0f, 1.0f, 0.0f);  // unit cylinder
     nvmath::vec3 dir = nvmath::normalize(p1 - p0);      // direction of segment that is currently calculated
     nvmath::vec3 v = nvmath::cross(unit, dir);
@@ -818,12 +817,10 @@ std::vector<HelloVulkan::Aabb> HelloVulkan::addCluster(const uint32_t i, const u
     nvmath::mat4f trans =
             nvmath::mat4f((-v.z * v.z - v.y * v.y) * safe + 1.0f, v.x * v.y * safe + v.z,
                           v.x * v.z * safe - v.y, 0.0f,
-                          (v.x * v.y * safe - v.z) * scale, ((-v.z * v.z - v.x * v.x) * safe + 1.0f) * scale,
-                          (v.y * v.z * safe + v.x) * scale, 0.0f, v.x * v.z * safe + v.y,
+                          (v.x * v.y * safe - v.z), ((-v.z * v.z - v.x * v.x) * safe + 1.0f),
+                          (v.y * v.z * safe + v.x), 0.0f, v.x * v.z * safe + v.y,
                           v.y * v.z * safe - v.x,
-                          (-v.y * v.y - v.x * v.x) * safe + 1.0f, 0.0f, p0.x, p0.y, p0.z, 1.0f);
-    m_trans.push_back(trans);
-    m_clusters.push_back(cluster);
+                          (-v.y * v.y - v.x * v.x) * safe + 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f);
     nvmath::mat4f trans_inv = nvmath::invert(trans);
 
     std::vector<Aabb> hairAabbs;
@@ -835,15 +832,23 @@ std::vector<HelloVulkan::Aabb> HelloVulkan::addCluster(const uint32_t i, const u
         nvmath::vec3f begin = trans_inv * hair.v0.p;
         nvmath::vec3f end = trans_inv * hair.v1.p;
         nvmath::vec3 a = end - begin;
-        nvmath::vec3 extent = nvmath::vec3(hair.thickness / 2.0f) * (nvmath::vec3(1.0f) - nvmath::normalize(a));
+        nvmath::vec3 extent = nvmath::vec3(hair.thickness) * (nvmath::vec3(1.0f) - nvmath::normalize(a));
         max = nvmath::nv_max(max, nvmath::nv_max(begin + extent, end + extent));
         min = nvmath::nv_min(min, nvmath::nv_min(begin - extent, end - extent));
     }
+    nvmath::vec3 range = max - min;
+    nvmath::mat4 scale = nvmath::mat4(range.x, 0.0f, 0.0f, 0.0f,
+                                      0.0f, range.y, 0.0f, 0.0f,
+                                      0.0f, 0.0f, range.z, 0.0f,
+                                      min.x, min.y,  min.z, 1.0f);
+    trans *= scale;
+    m_trans.push_back(trans);
+    m_clusters.push_back(cluster);
     hairAabbs.emplace_back(Aabb{min, max});
 
-    m_clustersAabbBuffer.emplace_back(m_alloc.createBuffer(
-            cmdBuf, hairAabbs, VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT |
-                               VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR));
+//    m_clustersAabbBuffer.emplace_back(m_alloc.createBuffer(
+//            cmdBuf, hairAabbs, VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT |
+//                               VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR));
     return hairAabbs;
 }
 
@@ -890,15 +895,20 @@ void HelloVulkan::createBottomLevelAS()
 
     m_hairsBuffer = m_alloc.createBuffer(cmdBuf, m_hairs, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
     m_clustersBuffer = m_alloc.createBuffer(cmdBuf, m_clusters, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
+    hairAabbs.clear();
+    hairAabbs.emplace_back(Aabb{{0.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 1.0f}});
+    m_clustersAabbBuffer.emplace_back(m_alloc.createBuffer(
+            cmdBuf, hairAabbs, VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT |
+                               VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR));
     genCmdBuf.submitAndWait(cmdBuf);
     // Debug information
     m_debug.setObjectName(m_hairsBuffer.buffer, "hairs");
 
-    for (int i = 0; i < m_clusters.size(); ++i)
-    {
-        auto blasHair = hairToVkGeometryKHR(i);
+//    for (int i = 0; i < m_clusters.size(); ++i)
+//    {
+        auto blasHair = hairToVkGeometryKHR(0);
         allBlas.emplace_back(blasHair);
-    }
+//    }
 
     m_rtBuilder.buildBlas(allBlas, VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR);
 }
@@ -920,11 +930,10 @@ void HelloVulkan::createTopLevelAS()
     // hairs
     for (uint32_t i = 0; i < m_clusters.size(); ++i)
     {
-        const auto& m_cluster = m_clusters[i];
         nvvk::RaytracingBuilderKHR::Instance rayInst;
         rayInst.transform = m_trans[i];
         rayInst.instanceCustomId = static_cast<uint32_t>(i);  // gl_InstanceCustomIndexEXT
-        rayInst.blasId = static_cast<uint32_t>(i);
+        rayInst.blasId = 0;
         rayInst.hitGroupId = 1;
         rayInst.flags = VK_GEOMETRY_INSTANCE_TRIANGLE_FACING_CULL_DISABLE_BIT_KHR;
         tlas.emplace_back(rayInst);
